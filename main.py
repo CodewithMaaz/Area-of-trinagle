b5ae9b4a9cda2515570309b1a5d086f55b397ed3
Maaz Ahmed <93466155+CodewithMaaz@users.noreply.github.com>
2021-11-21 17:15:01 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+#calculate formula of  triangle 
+# this code has been made by code with maaz
+print(" Triangle area  calculator 1.0 ")
+print("Made by code with maaz :)")
+
+heigth = input("Enter heigth ")
+heigth = int(heigth)
+
+Base = input("Enter base ")
+Base = int(Base)
+
+print("Wait we are calculating area :)" )
+
+Answer = heigth * Base * 0.5
+
+print(Answer)
+
+
